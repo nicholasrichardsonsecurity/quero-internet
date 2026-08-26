4e1929a5710516891766df7e92f2982b72e7d7ba
Nicholas Richardson <nicholasrichardsonsilva@gmail.com>
2026-08-25 22:38:27 -0300

    feat: load beneficiaries domain

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { BeneficiariesModule } from './beneficiaries/beneficiaries.module';
 import { DatabaseModule } from './database/database.module';
 import { HealthController } from './health/health.controller';
 
 @Module({
-  imports: [DatabaseModule, AuthModule],
+  imports: [DatabaseModule, AuthModule, BeneficiariesModule],
   controllers: [HealthController]
 })
 export class AppModule {}
