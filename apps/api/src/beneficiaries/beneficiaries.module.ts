6b92250c7305612d591386d483d53964141fb009
Nicholas Richardson <nicholasrichardsonsilva@gmail.com>
2026-08-26 00:04:48 -0300

    feat: register FTTH feasibility API

--- apps/api/src/beneficiaries/beneficiaries.module.ts
+++ apps/api/src/beneficiaries/beneficiaries.module.ts
@@ -4,12 +4,14 @@ import { ApplicationsController } from './applications.controller';
 import { BeneficiariesController } from './beneficiaries.controller';
 import { BeneficiariesService } from './beneficiaries.service';
 import { EligibilityReviewService } from './eligibility-review.service';
+import { FtthFeasibilityController } from './ftth-feasibility.controller';
+import { FtthFeasibilityService } from './ftth-feasibility.service';
 import { ReferralsController } from './referrals.controller';
 import { ReferralsService } from './referrals.service';
 
 @Module({
   imports: [AuthModule],
-  controllers: [BeneficiariesController, ApplicationsController, ReferralsController],
-  providers: [BeneficiariesService, EligibilityReviewService, ReferralsService]
+  controllers: [BeneficiariesController, ApplicationsController, ReferralsController, FtthFeasibilityController],
+  providers: [BeneficiariesService, EligibilityReviewService, ReferralsService, FtthFeasibilityService]
 })
 export class BeneficiariesModule {}
