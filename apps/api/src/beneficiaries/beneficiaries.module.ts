c6d433cdf1791874802e6792929b10abb39b5291
Nicholas Richardson <nicholasrichardsonsilva@gmail.com>
2026-08-25 23:49:43 -0300

    feat: register referral workflow

--- apps/api/src/beneficiaries/beneficiaries.module.ts
+++ apps/api/src/beneficiaries/beneficiaries.module.ts
@@ -4,10 +4,12 @@ import { ApplicationsController } from './applications.controller';
 import { BeneficiariesController } from './beneficiaries.controller';
 import { BeneficiariesService } from './beneficiaries.service';
 import { EligibilityReviewService } from './eligibility-review.service';
+import { ReferralsController } from './referrals.controller';
+import { ReferralsService } from './referrals.service';
 
 @Module({
   imports: [AuthModule],
-  controllers: [BeneficiariesController, ApplicationsController],
-  providers: [BeneficiariesService, EligibilityReviewService]
+  controllers: [BeneficiariesController, ApplicationsController, ReferralsController],
+  providers: [BeneficiariesService, EligibilityReviewService, ReferralsService]
 })
 export class BeneficiariesModule {}
