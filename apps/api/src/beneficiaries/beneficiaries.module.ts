31b0f6e5237e990774024d96e7e53bb170d5b736
Nicholas Richardson <nicholasrichardsonsilva@gmail.com>
2026-08-28 15:11:17 -0300

    feat: register installation workflow

--- apps/api/src/beneficiaries/beneficiaries.module.ts
+++ apps/api/src/beneficiaries/beneficiaries.module.ts
@@ -6,12 +6,20 @@ import { BeneficiariesService } from './beneficiaries.service';
 import { EligibilityReviewService } from './eligibility-review.service';
 import { FtthFeasibilityController } from './ftth-feasibility.controller';
 import { FtthFeasibilityService } from './ftth-feasibility.service';
+import { InstallationController } from './installation.controller';
+import { InstallationService } from './installation.service';
 import { ReferralsController } from './referrals.controller';
 import { ReferralsService } from './referrals.service';
 
 @Module({
   imports: [AuthModule],
-  controllers: [BeneficiariesController, ApplicationsController, ReferralsController, FtthFeasibilityController],
-  providers: [BeneficiariesService, EligibilityReviewService, ReferralsService, FtthFeasibilityService]
+  controllers: [
+    BeneficiariesController,
+    ApplicationsController,
+    ReferralsController,
+    FtthFeasibilityController,
+    InstallationController
+  ],
+  providers: [BeneficiariesService, EligibilityReviewService, ReferralsService, FtthFeasibilityService, InstallationService]
 })
 export class BeneficiariesModule {}
