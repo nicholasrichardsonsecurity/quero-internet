e91643b94e7552d8fc5d5b0a403a47480148182f
Nicholas Richardson <nicholasrichardsonsilva@gmail.com>
2026-09-01 12:25:06 -0300

    feat: conectar módulo de dashboard na API

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { BeneficiariesModule } from './beneficiaries/beneficiaries.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 import { DatabaseModule } from './database/database.module';
 import { HealthController } from './health/health.controller';
 
 @Module({
-  imports: [DatabaseModule, AuthModule, BeneficiariesModule],
+  imports: [DatabaseModule, AuthModule, BeneficiariesModule, DashboardModule],
   controllers: [HealthController]
 })
 export class AppModule {}
